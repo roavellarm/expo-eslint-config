5d5c1b243ce9364af2b6119d8b8d5245c0d12104
Rodrigo Avellar <roavellarm@gmail.com>
2020-08-16 01:04:03 -0300

    Test2

--- src/pages/Langing.tsx
+++ src/pages/Langing.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native';
-import styles from "./styles"
+import { Text, View } from 'react-native'
+import styles from './styles'
 
 export default function Landing() {
   return (
